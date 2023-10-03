0fae643f5a95ebca92a6a2d3a812c49c6d171b0e
Shahab Nasabeh <hichman662@gmail.com>
2023-10-03 21:47:06 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MoSIoT-Expert',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
